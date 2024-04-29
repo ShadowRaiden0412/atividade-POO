617851ec22abd71be6f3d74094ba61b42ff1e0ff
ShadowRaiden0412 <166518389+ShadowRaiden0412@users.noreply.github.com>
2024-04-29 09:27:37 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,23 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+// Programa de teste
+ 
+        public class main {
+            public static void main(String[] args) {
+                Fornecedor fornecedor = new Fornecedor("Fornecedor1", "EnderecoFornecedor1", "123456789", 5000, 2000);
+                System.out.println("Nome do fornecedor: " + fornecedor.getNome());
+                System.out.println("Saldo do fornecedor: " + fornecedor.obterSaldo());
+            }
+        }
+          
+      }
+
+
+
+  
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
---
+++ herança.java
@@ -0,0 +1,97 @@
+class Pessoa {
+    private String nome;
+    private String endereco;
+    private String telefone;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(String nome, String endereco, String telefone) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.telefone = telefone;
+    }
+
+    // Add at least two more constructors here as per your requirement
+
+    // Getters and Setters for attributes
+
+}
+
+class Fornecedor extends Pessoa {
+    private double valorCredito;
+    private double valorDivida;
+
+    public Fornecedor(String nome, String endereco, String telefone, double valorCredito, double valorDivida) {
+        super(nome, endereco, telefone);
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    public double obterSaldo() {
+        return valorCredito - valorDivida;
+    }
+}
+
+class Empregado extends Pessoa {
+    private int codigoSetor;
+    private double salarioBase;
+    private double imposto;
+
+    public Empregado(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto) {
+        super(nome, endereco, telefone);
+        this.codigoSetor = codigoSetor;
+        this.salarioBase = salarioBase;
+        this.imposto = imposto;
+    }
+
+    public double calcularSalario() {
+        return salarioBase - (salarioBase * imposto / 100);
+    }
+}
+
+class Administrador extends Empregado {
+    private double ajudaDeCusto;
+
+    public Administrador(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto, double ajudaDeCusto) {
+        super(nome, endereco, telefone, codigoSetor, salarioBase, imposto);
+        this.ajudaDeCusto = ajudaDeCusto;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + ajudaDeCusto;
+    }
+}
+
+class Operario extends Empregado {
+    private double valorProducao;
+    private double comissao;
+
+    public Operario(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto, double valorProducao, double comissao) {
+        super(nome, endereco, telefone, codigoSetor, salarioBase, imposto);
+        this.valorProducao = valorProducao;
+        this.comissao = comissao;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + (valorProducao * comissao / 100);
+    }
+}
+
+class Vendedor extends Empregado {
+    private double valorVendas;
+    private double comissao;
+
+    public Vendedor(String nome, String endereco, String telefone, int codigoSetor, double salarioBase, double imposto, double valorVendas, double comissao) {
+        super(nome, endereco, telefone, codigoSetor, salarioBase, imposto);
+        this.valorVendas = valorVendas;
+        this.comissao = comissao;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario() + (valorVendas * comissao / 100);
+    }
+}
